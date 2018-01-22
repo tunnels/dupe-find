6f88bd897f3033b94428b33b83771f07bbfcc80c
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2018-01-22 00:45:41 -0500

    method to calculate percentages of precalculated rgb values implemented

--- main.c
+++ main.c
@@ -8,11 +8,16 @@
 #define COMPONENT_MAX_VALUE 256 
 
 unsigned char *initializeImage(char *image_location, int *width, int *height, int *components_per_pixel, int set_components_value);
-void rgbComponentMethod(long *base_rgb_array, long *comparison_rgb_array, unsigned char *base_image, unsigned char *comparison_image, int width_base, int width_comparison, int height_base, int height_comparison, int num_pixels_comparison, int components_per_pixel_base, int components_per_pixel_comparison);
+void rgbComponentMethodStoreInArrays(long *base_rgb_array, long *comparison_rgb_array, unsigned char *base_image, unsigned char *comparison_image, int width_base, int width_comparison, int height_base, int height_comparison, int num_pixels_comparison, int components_per_pixel_base, int components_per_pixel_comparison);
+void rgbComponentMethodCalculatePercentages(long *base_rgb_array, long *comparison_rgb_array, int width_comparison, int height_comparison, int components_per_pixel_comparison);
 
 int main(void)
 {
-	// methods: checksum, rgb component array, tbc.
+	/*
+	todo:
+		methods: checksum, rgb component array, tbc.
+		add support for multiple threads
+	*/
 
 	int width_base, height_base, components_per_pixel_base;
 	int width_comparison, height_comparison, components_per_pixel_comparison;
@@ -37,9 +42,18 @@ int main(void)
 	printf("image a\nw:%d, h:%d, c:%d\n", width_base, height_base, components_per_pixel_base);
 	printf("\nimage b\nw:%d, h:%d, c:%d\n\n", width_comparison, height_comparison, components_per_pixel_comparison);
 
-	rgbComponentMethod(base_rgb_array, comparison_rgb_array, base_image, comparison_image, width_base, width_comparison,
+	rgbComponentMethodStoreInArrays(base_rgb_array, comparison_rgb_array, base_image, comparison_image, width_base, width_comparison,
 		height_base, height_comparison, num_pixels_comparison, components_per_pixel_base, components_per_pixel_comparison);
-	
+	rgbComponentMethodCalculatePercentages(base_rgb_array, comparison_rgb_array, width_comparison, height_comparison, components_per_pixel_comparison);
+
+	// todo: implement the method presented in pseudocode below
+	// if (*(base_image + i) != *(comparison_image + i) || 
+	// 	*(base_image + i + 1) != *(comparison_image + i + 1) || 
+	// 	*(base_image + i + 2) != *(comparison_image + i + 2)) {
+	// 	printf("not identical\n");
+	// 	break;
+	// }
+
 	// if (checkPercentages() is under threshold) {
 	// 	printf("\nthey seem identical\n");
 	// }
@@ -55,28 +69,17 @@ unsigned char *initializeImage(char *image_location, int *width, int *height, in
 	return stbi_load(image_location, width, height, components_per_pixel, set_components_value);
 }
 
-void rgbComponentMethod(long *base_rgb_array, long *comparison_rgb_array, unsigned char *base_image, unsigned char *comparison_image, int width_base, int width_comparison, int height_base, int height_comparison, int num_pixels_comparison, int components_per_pixel_base, int components_per_pixel_comparison)
+void rgbComponentMethodStoreInArrays(long *base_rgb_array, long *comparison_rgb_array, unsigned char *base_image, unsigned char *comparison_image, int width_base, int width_comparison, int height_base, int height_comparison, int num_pixels_comparison, int components_per_pixel_base, int components_per_pixel_comparison)
 {
+	// skip adjacent pixels or only get 1 out of every 10 pixels or something, requires testing for (configurable) sweet spot
+	// rgbComponentMethodBuildShrinkPixelsToCheck();
+
 	if (width_base == width_comparison && height_base == height_comparison) {
 		if (components_per_pixel_base == components_per_pixel_comparison) {
-			for (int i = 0; i < (num_pixels_comparison * components_per_pixel_comparison); i += components_per_pixel_comparison) {
-				// method 1
-				// if (*(base_image + i) != *(comparison_image + i) || 
-				// 	*(base_image + i + 1) != *(comparison_image + i + 1) || 
-				// 	*(base_image + i + 2) != *(comparison_image + i + 2)) {
-				// 	printf("not identical\n");
-				// 	break;
-				// }
-
-				// method 2
-				// loop through componenents rather than assume there will be 3 (r,g,b) components + alpha
-				base_rgb_array[0] += *(base_image + i); base_rgb_array[1] += *(base_image + i + 1); base_rgb_array[2] += *(base_image + i + 2);
-				comparison_rgb_array[0] += *(comparison_image + i); comparison_rgb_array[1] += *(comparison_image + i + 1); comparison_rgb_array[2]+= *(comparison_image + i + 2);
-
-				// lazy method
-				// if (i > 200) {
-				// 	break;
-				// }
+			for (int i = 0; i < (num_pixels_comparison * components_per_pixel_comparison); i++) {
+				// there are only components_per_pixel_base elements in the rgb arrays
+				base_rgb_array[i % components_per_pixel_base] += base_image[i];
+				comparison_rgb_array[i % components_per_pixel_base] += comparison_image[i];
 			}
 		}
 		else {
@@ -91,13 +94,19 @@ void rgbComponentMethod(long *base_rgb_array, long *comparison_rgb_array, unsign
 	
 	printf("number of reds [base]: %ld, greens: %ld, blues: %ld\n", base_rgb_array[0], base_rgb_array[1], base_rgb_array[2]);
 	printf("number of reds [comparison]: %ld, greens: %ld, blues: %ld\n\n", comparison_rgb_array[0], comparison_rgb_array[1], comparison_rgb_array[2]);
+}
 
-	printf("percentage of reds [base]: %f, greens: %f, blues: %f\n", 
-		(base_rgb_array[0]/(4*100*100*256.0))*100,
-		(base_rgb_array[1]/(4*100*100*256.0))*100,
-		(base_rgb_array[2]/(4*100*100*256.0))*100);
-	printf("percentage of reds [comparison]: %f, greens: %f, blues: %f\n", 
-		(comparison_rgb_array[0]/(4*100*100*256.0))*100, 
-		(comparison_rgb_array[1]/(4*100*100*256.0))*100, 
-		(comparison_rgb_array[2]/(4*100*100*256.0))*100);	
+void rgbComponentMethodCalculatePercentages(long *base_rgb_array, long *comparison_rgb_array, int width_comparison, int height_comparison, int components_per_pixel_comparison)
+{
+	double denominator = width_comparison * height_comparison * components_per_pixel_comparison * COMPONENT_MAX_VALUE + 0.0;
+
+	for (int i = 0; i < components_per_pixel_comparison; i++) {
+		double base_calculated_value = (base_rgb_array[i] / denominator) * 100;
+		double comparison_calculated_value = (comparison_rgb_array[i] / denominator) * 100;
+
+		printf("comonent %d: ", i);
+		printf("[base] = %f  ", base_calculated_value);
+		printf("[comparison] = %f  ", comparison_calculated_value);
+		printf("[difference] = %f\n", (comparison_calculated_value - base_calculated_value));
+	}
 }
