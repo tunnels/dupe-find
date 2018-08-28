c098f8d6435304eb0f0281dfce33e16446040ab8
tunnels <tunnels@tuta.io>
2018-08-27 20:56:59 -0400

    moved image struct into df.h

--- includes/df.h
+++ includes/df.h
@@ -2,15 +2,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <dirent.h>
-#include "image.h"
+// #include "image.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-
 // each channel in each pixel will have a value from 1 to CHANNEL_MAX_VALUE
 #define CHANNEL_MAX_VALUE 256.0
 #define MAX_IMAGES_PER_DIR 2000
+
+typedef struct image_struct_t {
+	unsigned char *image_data;
+	char location[FILENAME_MAX * 2];
+	int width;
+	int height;
+	int number_of_pixels;
+	int channels_per_pixel;
+	long long *channels;
+	struct image_struct_t *next;
+} Image;
+
 
 bool verifyParams(int argc, char *source_directory);
 void printImageDetails(Image *image);
